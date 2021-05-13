6f4b6d23a9ec06eb4aa6615bf6e0f66454053ff4
Nadav Ben Haim <nadavbh@microsoft.com>
2021-05-13 12:56:21 +0300

    Test

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "prefix" {
+  description = "A prefix used for all resources in this example"
+}
+
+variable "location" {
+  description = "The Azure Region in which all resources in this example should be provisioned"
+}
+
+variable "nodecount" {
+  description = "how many nodes"
+}
